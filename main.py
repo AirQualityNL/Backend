efc7fc35764408bc7909bfb8677c22934b5cb930
Ryan Bours <ryan.bours@gmail.com>
2024-06-07 10:24:01 +0200

    updated requirements using pipenv

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ from service.pollution import PollutantService
 
 app = FastAPI()
 
+@app.get('/', status_code=418 )
+async def root():
+    return {"message": "I'm a teapot"}
 
 @app.get("/pollutants/{target_datetime}")
 async def get(target_datetime: str, lat: float = 51.4231, lon: float = 5.4623):
     pollutant_service = PollutantService()
     current_pollutants = pollutant_service.get(target_datetime, lat, lon)
     return current_pollutants
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
