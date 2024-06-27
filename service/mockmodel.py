51ffe5b42cfc7774ce64ea1a792012cc798a7e35
airell <i450046@fhict.nl>
2024-06-27 12:42:53 +0200

    Replace mock data

--- service/mockmodel.py
+++ service/mockmodel.py
@@ -6,8 +6,8 @@ class MockModelService:
 
     def predict(self):
         random_index = random.randint(0, self.data.shape[0] - 1)
-        no2 = [self.data[random_index][0][1], self.data[random_index][0][3]]
-        pm25 = [self.data[random_index][0][1], self.data[random_index][0][3]]
-        pm10 = [self.data[random_index][0][1], self.data[random_index][0][3]]
-        o3 = [self.data[random_index][0][1], self.data[random_index][0][3]]
+        no2 = [self.data[random_index][0][0], self.data[random_index][1][0]]
+        pm25 = [self.data[random_index][0][3], self.data[random_index][1][3]]
+        pm10 = [self.data[random_index][0][2], self.data[random_index][1][2]]
+        o3 = [self.data[random_index][0][4], self.data[random_index][1][4]]
         return pm25, pm10, no2, o3
